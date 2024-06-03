61848b4b4f5079a244c0e299555079d5ea593541
Nicole <z5206727@unsw.edu.au>
2024-06-03 12:57:38 +1000

    solutions

--- week_1/hello_world.c
+++ week_1/hello_world.c
@@ -1,11 +1,11 @@
 // Basic Hello World Program
-// Nicole, 24T32Week 1
+// Nicole, 24T2 Week 1
 
 #include <stdio.h>
 
 int main(void) {
     
-    printf("Hello World\n");
+    printf("/\\___/\\\n(=' w '=)\n");
 
     return 0;
 }
