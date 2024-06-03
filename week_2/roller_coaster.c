edf90d913cf3a818ff39043bdedddd93325b0054
Nicole <z5206727@unsw.edu.au>
2024-06-03 14:11:56 +1000

    solutions

--- week_2/roller_coaster.c
+++ week_2/roller_coaster.c
@@ -20,7 +20,40 @@
 
 #include <stdio.h>
 
+#define MIN_RIDE_HEIGHT 200
+#define RIDE_ALONE_HEIGHT 300
+
 int main(void) {
+
+	double height;
+	printf("How tall are you? ");
+	scanf("%lf", &height);
+
+	// if (height <= 0) {
+	// 	printf("Error: enter a valid height\n");
+	// } else {
+	// 	if (height >= MIN_RIDE_HEIGHT && height < RIDE_ALONE_HEIGHT) {
+	// 		printf("You are too short but you can ride with an adult\n");
+	// 	} else if (height < MIN_RIDE_HEIGHT) {
+	// 		printf("You are too short!\n");
+	// 	}
+
+	// 	if (height >= RIDE_ALONE_HEIGHT) {
+	// 		printf("You are tall enough for the ride!");
+	// 	}
+	// }
+	
+	if (height <= 0) {
+		printf("Error: enter a valid height\n");
+	} else if (height >= MIN_RIDE_HEIGHT && height < RIDE_ALONE_HEIGHT) {
+		printf("You are too short but you can ride with an adult\n");
+	} else if (height < MIN_RIDE_HEIGHT) {
+		printf("You are too short!\n");
+	} else {
+		printf("You are tall enough for the ride!\n");
+	}
+
+	
 	
 	return 0;
 }
--- week_2/variables.c
+++ week_2/variables.c
@@ -4,7 +4,24 @@
 
 #include <stdio.h>
 
+#define PI 3.1415
+
 int main(void) {
+
+	// make variables to hold integers
+	int radius;
+	// double for Pi
+
+	// get radius from user
+	printf("Please enter a radius: ");
+	scanf("%d", &radius);
+
+	// calculate area of circle with pi*r^2
+	double area = pi*radius*radius;
+
+	// print out the answer
+	printf("Area: %lf\n", area);
+
 
 	return 0;
 }
