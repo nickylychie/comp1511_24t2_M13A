230fa4fff3696f6c537e7bbbc6d1899203cf6f58
Nicole <z5206727@unsw.edu.au>
2024-06-16 16:45:56 +1000

    Ready for tutorial

---
+++ week_4/functions.c
@@ -0,0 +1,20 @@
+// Nicole, 24T2 Week 4
+// This program uses a function to add 2 integers together
+
+#include <stdio.h>
+
+// TODO: Function protoype
+
+int main() {
+    int result;
+    int num1 = 5;
+    int num2 = 3;
+
+    // TODO: Function call 
+
+    printf("Result: %d\n", result);
+    return 0;
+}
+
+// TODO: Function definition
+
