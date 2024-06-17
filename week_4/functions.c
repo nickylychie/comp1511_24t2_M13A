1e65a72a50a361cc0b2d65f82ebfc17717318bcc
Nicole Luong <nicoleluong99@gmail.com>
2024-06-17 14:12:58 +1000

    Tutorial code

--- week_4/functions.c
+++ week_4/functions.c
@@ -3,14 +3,22 @@
 
 #include <stdio.h>
 
+int addition(int a, int b);
+
 int main() {
     int result;
     int num1 = 5;
     int num2 = 3;
 
     // TODO: Add 2 integers together
+    result = addition(num1, num2);
 
     printf("Result: %d\n", result);
     return 0;
 }
 
+int addition(int a, int b) {
+    int result = a + b;
+
+    return result;
+}
